6f185215fd7bf0317be78a6be3bfe8c1487d2977
Ashutosh <ashutosh.chauhan@napses.com>
2024-07-17 19:36:16 +0530

    refactored exceptions

---
+++ src/main/java/main/exceptions/UserErrorResponse.java
@@ -0,0 +1,22 @@
+package main.exceptions;
+
+public class UserErrorResponse {
+    private int code;
+    private String message;
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
